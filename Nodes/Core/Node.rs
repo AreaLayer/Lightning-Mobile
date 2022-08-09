54ec5232647e465b11a9a6f7bc5afb6b12d64388
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-08-09 06:21:16 -0300

    Create Node.rs

---
+++ Nodes/Core/Node.rs
@@ -0,0 +1,4 @@
+use bitcoin::blockdata::script::{Script,Builder};
+use bitcoin::blockdata::transaction::{Transaction, EcdsaSighashType};
+use bitcoin::util::sighash;
+use bitcoin::consensus::encode;
