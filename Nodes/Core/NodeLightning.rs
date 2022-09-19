b08824eaa5d880bb3de88c0707ae286bb744d60f
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-09-19 06:42:22 -0300

    Update NodeLightning.rs

--- Nodes/Core/NodeLightning.rs
+++ Nodes/Core/NodeLightning.rs
@@ -2,9 +2,9 @@ use bitcoin::blockdata::script::{Script,Builder};
 use bitcoin::blockdata::transaction::{Transaction, EcdsaSighashType};
 use bitcoin::util::sighash;
 use bitcoin::consensus::encode;
-use lightning:: blockdata:: script
-use lightning::channel:: script
-use lightning::closechanel:: script
-use lightning::testnet::script
-use lightning:mainnet::sript
+use lightning:: blockdata:: script;
+use lightning::channel:: script;
+use lightning::closechanel:: script;
+use lightning::testnet::script;
+use lightning:mainnet::sript;
 use lightning::testnet::script
