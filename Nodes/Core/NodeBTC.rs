3c551cb2b4e6d43d85ef70a32441756b3872cd81
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-10-03 07:39:37 -0300

    Update NodeBTC.rs

--- Nodes/Core/NodeBTC.rs
+++ Nodes/Core/NodeBTC.rs
@@ -2,3 +2,5 @@ use bitcoin::blockdata::script::{Script,Builder};
 use bitcoin::blockdata::transaction::{Transaction, EcdsaSighashType};
 use bitcoin::util::sighash;
 use bitcoin::consensus::encode;
+use bitcoin:network::testnet;
+use bitcoin:network:signet;
